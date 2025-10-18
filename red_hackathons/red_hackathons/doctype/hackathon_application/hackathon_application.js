b6631a5d62dadbd2ecc68ca3ab0643a7162e3ae8
NjengaSheba <njengasheba@gmail.com>
2025-10-18 08:02:25 +0300

    score totals for round 2

--- red_hackathons/red_hackathons/doctype/hackathon_application/hackathon_application.js
+++ red_hackathons/red_hackathons/doctype/hackathon_application/hackathon_application.js
@@ -1,8 +1,70 @@
 // Copyright (c) 2025, Kelvin Njenga and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Hackathon Application", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on("Hackathon Application", {
+    refresh(frm) {
+        calc_total_score(frm);
+    },
+    validate: function(frm, cdt,cdn) {
+        calc_total_score(frm);
+        
+    },
+
+});
+
+frappe.ui.form.on("Hackathon Round Entry", {
+    score(frm, cdt, cdn){
+        calc_total_score(frm);
+        run_validations(frm, cdt, cdn);
+
+        
+    }
+});
+
+
+
+function calc_total_score(frm) {
+    let total_score = 0;
+    // Round ONE
+
+    // Round TWO
+    (frm.doc.round_two || []).forEach(row => {
+        total_score += row.score || 0;
+    });
+    frm.set_value('score_2', total_score);
+};
+
+function run_validations(frm, cdt, cdn) {
+    // Check weight of score
+
+    let row = locals[cdt][cdn];
+
+    frappe.db.get_value("Hackathon Selection Criteria", row.criteria, "weight")
+    .then( r=> {
+        if (!r || !r.message) {
+            return;
+        }
+
+        let weight = r.message.weight;
+        let score = row.score;
+
+        if (score > weight) {
+            frappe.msgprint({
+                title: __("Invalid Score"),
+                message: __("The weight for this criteria is between 0 and {0}", [weight]), 
+                indicator: "red"
+            });
+
+            frappe.model.set_value(cdt, cdn, "score", "");
+        }
+        
+    })
+
+    
+
+
+};
+
+
+
