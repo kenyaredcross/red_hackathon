6a247d6ce4267deafd0fe7c4ede45adc1a819a8b
Kenya Red Cross Society <151393506+kenyaredcross@users.noreply.github.com>
2026-03-12 11:58:42 +0300

    commented out

--- red_hackathons/patches/update_round_one_scores.py
+++ red_hackathons/patches/update_round_one_scores.py
@@ -1,18 +1,18 @@
-import frappe
+# import frappe
 
-def execute():
-    applications = frappe.get_all("Hackathon Application", fields=["name"])
+# def execute():
+#     applications = frappe.get_all("Hackathon Application", fields=["name"])
 
-    for app in applications:
-        doc = frappe.get_doc("Hackathon Application", app.name)
-        total_score = 0
-        total_rows = len(doc.scores_round_one or [])
+#     for app in applications:
+#         doc = frappe.get_doc("Hackathon Application", app.name)
+#         total_score = 0
+#         total_rows = len(doc.scores_round_one or [])
 
-        if total_rows:
-            for row in doc.scores_round_one:
-                total_score += (row.score or 0)
+#         if total_rows:
+#             for row in doc.scores_round_one:
+#                 total_score += (row.score or 0)
 
-        doc.round_one_total_score = total_score
-        doc.db_update()
+#         doc.round_one_total_score = total_score
+#         doc.db_update()
 
-    frappe.db.commit()
+#     frappe.db.commit()
